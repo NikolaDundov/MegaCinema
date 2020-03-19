367f13d670a764c9272f9c66818089fa0cee04db
NikolaDundov <nikoladundov@gmail.com>
2020-03-19 21:23:04 +0200

    Add RolesSeeder for admin user

--- Data/MegaCinema.Data/Seeding/RolesSeeder.cs
+++ Data/MegaCinema.Data/Seeding/RolesSeeder.cs
@@ -8,6 +8,7 @@
     using MegaCinema.Data.Models;
 
     using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
 
     internal class RolesSeeder : ISeeder
@@ -15,11 +16,13 @@
         public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
-            await SeedRoleAsync(roleManager, GlobalConstants.AdministratorRoleName);
+            await SeedRoleAsync(roleManager, userManager, GlobalConstants.AdministratorRoleName);
         }
 
-        private static async Task SeedRoleAsync(RoleManager<ApplicationRole> roleManager, string roleName)
+        private static async Task SeedRoleAsync(
+            RoleManager<ApplicationRole> roleManager, UserManager<ApplicationUser> userManager, string roleName)
         {
             var role = await roleManager.FindByNameAsync(roleName);
             if (role == null)
@@ -28,6 +31,25 @@
                 if (!result.Succeeded)
                 {
                     throw new Exception(string.Join(Environment.NewLine, result.Errors.Select(e => e.Description)));
+                }
+            }
+
+            if (!await userManager.Users.AnyAsync())
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = "admin@mail.com",
+                    Email = "admin@mail.com",
+                    EmailConfirmed = true,
+                };
+
+                var password = "123456";
+
+                var result = await userManager.CreateAsync(user, password);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, GlobalConstants.AdministratorRoleName);
                 }
             }
         }
